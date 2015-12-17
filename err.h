ff414b84e5242f2ae5bc67cc251d7c48ca70a4f7
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2015-12-17 21:52:23 +0100

    err.h added

---
+++ err.h
@@ -0,0 +1,28 @@
+#ifndef ERR_H
+#define ERR_H
+#include <stdio.h>
+#include <signal.h>
+#include <errno.h>
+
+
+#define FATAL(...) fprintf(stderr,__VA_ARGS__)
+
+#define MEM_NOT_AVAIL(ptr,file_name) fprintf(stderr,"%s:bad memory allocation for %s pointer\n",file_name,ptr)
+
+#define file_err(A) if (!(A)) {printf("%s\n",strerror(EIO));}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
